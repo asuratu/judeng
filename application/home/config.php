33cca012cc5059a67d2cf9f50775f533ba408057
tuge <455224159@qq.com>
2018-05-07 15:19:07 +0800

    no message

--- application/home/config.php
+++ application/home/config.php
@@ -3,6 +3,10 @@
 return [
     'key'=>'b946bc0dbbde213e504bc1c32616a769',
     'body'=>'【朗企科技】您的验证码：%s。如非本人操作，请忽略。',
-    'outtime'=>30,//请求超时时间 30s
+    'outtime'=>300,//请求超时时间 300s
 
+    'client_id'=>'YXA6W_h-IFAUEeiIo4-HJ71spA',
+    'client_secret'=>'YXA6gGJZnrPrMqhseWBRlYQLX8i2DDQ',
+    'org_name'=>'1141180504253647',
+    'app_name'=>'xiaojudeng',
 ];
--- application/home/controller/Common.php
+++ application/home/controller/Common.php
@@ -12,45 +12,11 @@ class Common extends Controller
 
     }
 
-    /**
-     * @Title: getAreaList
-     * @Description: TODO 获取地区列表
-     */
-    public function getAreaList() {
-        if($this->request->isPost())
-        {
-            $data=input('post.');
-            $res=checkSign($data);
-            if($res['code']==0)
-            {
-                ajaxReturn($res);
-            }
-
-            //获取地区数组
-            $map['is_display'] = 1;
-            $list=db('area')->where($map)->field("`area_id`,`name`")->order("`sort` DESC")->select();
-                ajaxReturn(array('code'=>1, 'info'=>'ok','data'=>$list));
-        }
-    }
-
-    /**
-     * @Title: getHospitalList
-     * @Description: TODO 获取医院列表
-     */
-    public function getHospitalList() {
-        if($this->request->isPost())
-        {
-            $data=input('post.');
-            $res=checkSign($data);
-            if($res['code']==0)
-            {
-                ajaxReturn($res);
-            }
-
-            //获取医院数组
-            $map['is_display'] = 1;
-            $list=db('hospital')->where($map)->field("`hospital_id`,`hospital_name`")->order("`sort` DESC")->select();
-                ajaxReturn(array('code'=>1, 'info'=>'ok','data'=>$list));
-        }
+    public function countToken($token)
+    {
+        $map['only_token'] = $token;
+        $map['is_status'] = 0;
+        $num = db('doctor')->where($map)->count();
+        var_dump($num);die;
     }
 }
