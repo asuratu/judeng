b2dcc8a80e9d04323e4b92127685a6366470175d
tuge <455224159@qq.com>
2018-05-14 09:32:54 +0800

    no message

--- application/home/config.php
+++ application/home/config.php
@@ -1,6 +1,13 @@
 <?php
 //配置文件
 return [
+    'view_replace_str'  =>  [
+        '__PUBLIC__'=>__ROOT__.'/public',
+        '__CSS__' => __ROOT__.'/public/home/css',
+        '__JS__' => __ROOT__.'/public/home/js',
+        '__IMG__' => __ROOT__.'/public/home/images',
+        '--PUBLIC--' => __ROOT__.'/public',
+    ],
     'key'=>'b946bc0dbbde213e504bc1c32616a769',
     'body'=>'【朗企科技】您的验证码：%s。如非本人操作，请忽略。',
     'outtime'=>300,//请求超时时间 300s
@@ -16,5 +23,5 @@ return [
     'url'=>'http://localhost:7037',
 
     //药房相关参数
-    'lessCount'=>20,//药品库存预留量, 下单量大于该值无法下单
+    'lessCount'=>0,//药品库存预留量, 下单量大于该值无法下单
 ];
---
+++ public/home/js/common.js
@@ -0,0 +1,2 @@
+
+var deviceWidth = document.documentElement.clientWidth; if(deviceWidth > 750) deviceWidth = 750; document.documentElement.style.fontSize = deviceWidth / 7 + 'px';
